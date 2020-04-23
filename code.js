a2b6d35f7bd85ab0e35985b9961cc617e4e184d6
JahzielB <jahzielb@gmail.com>
2020-04-23 15:11:25 -0500

    unit tests for edge cases

--- code.js
+++ code.js
@@ -7,5 +7,9 @@ function helloWorld() {
 // }
 
 function sayHello(input) {
-    return "Hello, " + input + "!";
+    if (typeof input === "string") {
+        return "Hello, " + input + "!";
+    } else {
+        return "Hello, World!";
+    }
 }
